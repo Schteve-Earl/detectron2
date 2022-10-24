905901818c17a90a490e56838f7c094bc821007f
Schteve-Earl <searl0030@gmail.com>
2022-10-24 09:07:49 -0600

    changing the camera

--- demo/video_test.py
+++ demo/video_test.py
@@ -11,19 +11,19 @@ import cv2
 
 
 if __name__ == "__main__":
-    cam_color = cv2.VideoCapture(8)
-    cam_depth = cv2.VideoCapture(6)
+    cam_color = cv2.VideoCapture(4)
+    # cam_depth = cv2.VideoCapture(6)
 
     while True:
         ret, frame_color = cam_color.read()
-        ret, frame_depth = cam_depth.read()
+        # ret, frame_depth = cam_depth.read()
         
         # Show the captured image
         cv2.imshow('WebCamColor', frame_color)
-        cv2.imshow('WebCamDepth', frame_depth)
+        # cv2.imshow('WebCamDepth', frame_depth)
         
         # wait for the key and come out of the loop
         if cv2.waitKey(1) == ord('q'):
             break
-    cam.release()
+    cam_color.release()
     cv2.destroyAllWindows()
